f85689111daaa6b15bc121db2ff6f18d015dcdb6
Eugene Molodkin <burivuh@gmail.com>
2013-07-29 23:23:33 +0400

    Add some resources

--- example/example.js
+++ example/example.js
@@ -1,8 +1,7 @@
 ﻿angular.module('myApp', ['dg-api'])
     .config(function(dgApiProvider) {
         dgApiProvider.init({
-            key: 'ruzqvd2307',
-            version: '1.3'
+            key: 'ruzqvd2307'
         });
     })
     .controller('ExampleCtrl', function($scope, dgApi) {
@@ -13,4 +12,28 @@
             where: 'Москва'
         });
 
+        $scope.searchResult = dgApi.Search.query({
+            what: 'starbucks',
+            where: 'Москва'
+        }, function(data) {
+            // retrieve profile info for the first search result
+            if(data && data.result && data.result.length) {
+                $scope.profile = dgApi.Profile.get({
+                    id: data.result[0].id,
+                    hash: data.result[0].hash
+                }, function(profile) {
+                    console.log(profile);
+                });
+            }
+        });
+
+        $scope.searchInRubricResult = dgApi.SearchInRubric.query({
+            what: 'хостинг',
+            where: 'Москва'
+        });
+
+        $scope.searchAdsResult = dgApi.SearchAds.query({
+            what: 'окна',
+            where: 'Москва'
+        });
     });
--- src/angular-dg-api.js
+++ src/angular-dg-api.js
@@ -3,24 +3,95 @@
 
     var dgApiModule = angular.module("dg-api", ['ngResource']);
 
+    /**
+     * 2GIS API Provider
+     * @constructor
+     */
     function DgApiProvider() {
         var options = {
             output: 'jsonp',
+            version: '1.3',
             callback: 'JSON_CALLBACK'
-        };
+         };
 
+        /**
+         * Init provider
+         * @param {Object} opts
+         * @param {Object} opts.key Your 2GIS API key
+         */
         this.init = function(opts) {
             angular.extend(options, opts);
         };
 
         this.$get = function($resource) {
+            /**
+             * Profile resource
+             */
+            var Profile = $resource('http://catalog.api.2gis.ru/profile', options, {
+                get: {method: 'JSONP', isArray: false}
+            });
+
+            /**
+             * Register business connection for profile
+             */
+            Profile.prototype.registerBC = function() {
+                if(!this.register_bc_url) {
+                    return;
+                }
+
+                // Send GET request via script tag insertion
+                var script = document.createElement('script');
+                script.async = true;
+                script.src = this.register_bc_url;
+                script.onload = function() {
+                    document.body.removeChild(script);
+                };
+
+                document.body.appendChild(script);
+            };
+
             return {
+                /**
+                 * Projects resource
+                 * For a list of available parameters please see {@link http://api.2gis.ru/doc/firms/list/project-list/}
+                 */
                 Projects: $resource('http://catalog.api.2gis.ru/project/list', options, {
-                    query: {method:'JSONP', isArray:false}
+                    query: {method: 'JSONP', isArray: false}
                 }),
+
+                /**
+                 * Rubricator resource
+                 * For a list of available parameters please see {@link http://api.2gis.ru/doc/firms/list/rubricator/}
+                 */
                 Rubricator: $resource('http://catalog.api.2gis.ru/rubricator', options, {
-                    query: {method:'JSONP', isArray:false}
-                })
+                    query: {method: 'JSONP', isArray: false}
+                }),
+
+                /**
+                 * Search service
+                 * For a list of available parameters please see {@link http://api.2gis.ru/doc/firms/searches/search/}
+                 */
+                Search: $resource('http://catalog.api.2gis.ru/search', options, {
+                    query: {method: 'JSONP', isArray: false}
+                }),
+
+                /**
+                 * Search in rubric
+                 * {@link http://api.2gis.ru/doc/firms/searches/searchinrubric/}
+                 */
+                SearchInRubric: $resource('http://catalog.api.2gis.ru/searchinrubric', options, {
+                    query: {method: 'JSONP', isArray: false}
+                }),
+
+                /**
+                 * Search for Ads
+                 * {@link http://api.2gis.ru/doc/firms/searches/adssearch/}
+                 */
+                SearchAds: $resource('http://catalog.api.2gis.ru/ads/search', options, {
+                    query: {method: 'JSONP', isArray: false}
+                }),
+
+                Profile: Profile
             };
         };
 
