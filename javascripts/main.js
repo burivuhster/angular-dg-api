7ebef164b99c91f95917081f89d41d50b6b8378b
Eugene Molodkin <burivuh@gmail.com>
2013-07-30 22:40:03 +0400

    Add example

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,16 @@
-console.log('This would be the main JS file.');
+angular.module('dgExample', ['dg-api'])
+    .config(function(dgApiProvider) {
+        dgApiProvider.init({
+            key: 'ruzqvd2307'
+        })
+    })
+    .controller('MainCtrl', function($scope, dgApi) {
+        $scope.projects = dgApi.Projects.query();
+
+        $scope.search = function() {
+            $scope.searchResult = dgApi.Search.query({
+                what: $scope.query,
+                where: $scope.selectedProject
+            });
+        };
+    });
